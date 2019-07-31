482898bca160d280e6bead1395d4b704a17c91b2
Alex <work.alex966@gmail.com>
2019-07-31 17:45:23 +0300

    немного правок,немного аддаптива + скелет хедера

--- main.js
+++ main.js
@@ -2,4 +2,39 @@ document.addEventListener('DOMContentLoaded', () => {
     $(".paroller, [data-paroller-factor]").paroller({
         type: 'foreground'
     });
+
+    //adapt-menu
+
+    let selector = selector => document.querySelector(selector);
+    let selectorAll = selector => document.querySelectorAll(selector);
+
+    if(document.documentElement.clientWidth <= 1100) {
+        initMobileMenu();
+    }
+    
+    function initMobileMenu() {
+        const openMenuBtn = selector('.open-menu-btn');
+        const closeMenuBtn = selector('.close-menu-btn');
+        const mobileMenu = selector('.mobile-menu');
+        const mobileMenuOverlay = selector('.mobile-menu-overlay');
+
+        openMenuBtn.addEventListener('click', ()=> {
+            menuToggleClasses();
+        });
+
+        closeMenuBtn.addEventListener('click', ()=>{
+            menuToggleClasses();
+        });
+
+        mobileMenuOverlay.addEventListener('click', ()=>{
+            menuToggleClasses();
+        });
+
+        function menuToggleClasses() {
+            mobileMenu.classList.toggle('mobile-menu_active');
+            mobileMenuOverlay.classList.toggle('mobile-menu-overlay_active');
+            document.body.classList.toggle('no-overflow');
+        }
+        
+    }
 });
