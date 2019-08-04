ce05cf43f0091a0f1b5ab92478ae382d4f5e197b
Alex <work.alex966@gmail.com>
2019-08-04 22:35:40 +0300

    страница crm

---
+++ service-srm.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" type="text/css" href="css/slick.css"/>
+    <title>amoSRM</title>
+</head>
+<body>
+<header class="header header-srm">
+<?php include('template/header.php') ?>
+    <div class="container">
+        <section class="header-bottom">
+        <div class="small-info-service-about small-info-service-seo-about">
+            <h3 class="smal-info-service-title smal-info-service-seo-title smal-info-service-srm-title">Получайте максимум от своего бизнеса с <img class="srm-text" src="images/crm-text.png"></h3>
+            <div class="sml-info-service-we-top sml-info-service-seo-we-top sml-info-service-srm-we-top">amoCRM – полный комплекс инструментов, направленных на раскрытие потенциала вашего отдела продаж и повышение его эффективности</div>
+            <button class="btn-more-service-seo btn-more-service-srm">Подробнее</button>
+        </div>
+        </section>
+    </div>
+</header>   
+<section class="what-cmm">
+<div class="container container-what-smm">
+    <img class="wtah-smm-video" src="">
+</div>
+</section>
+<section class="amoCrm-helper">
+    <div class="container container-amoCrm-helper">
+        <h4 class="title amoCrm-helper-title">amoCRM -незаменимый помощник в отделе продаж</h4>
+        <img class="amoCrm-helper-img" src="images/crm-helper-fon.png">
+        <div class="wrapper-amoCrm-helper">
+            <span class="amoCrm-helper-sub-title">Клиент в центре внимания</span>
+            <span class="amoCrm-helper-text">Social Media Marketing - это комплекс инструментов, позволяющий найти и привлечь внимание целевой аудитории. Основная цель SMM повысить лояльность бренда перед вашей аудиторий, что неизменно повысит узнаваемость и принесет дополнительные продажи. Построим стратегию, которая позволит: повысить рост лояльности текущих и потенциальных клиентов сформирование имиджа бренда проинформировать аудиторию провести анализ обратной связи от пользователей отработать негативные отзывы и собрать положительные Для продвижения бренда используем такие площадки, как ВКонтакте, Facebook, Instagram, Одноклассники, LinkedIn, Twitter, YouTube и др. Секрет нашего успеха в индивидуальном подходе</span>
+            <button class="btn-more-service-seo btn-more-service-srm">Подробнее</button>
+        </div>
+    <div>
+</section>
+<section class="smm-info-1">
+<div class="container container-smm-info-1">
+    <h4 class="title smm-info-1-title">Digital воронка в помощники</h4>
+    <div class="wrapper-smm-info-1">
+        <div class="wrapper-smm-info-1-text">
+            <span class="smm-info-1-text">Social Media Marketing - это комплекс инструментов, позволяющий найти и привлечь внимание целевой аудитории. Основная цель SMM повысить лояльность бренда перед вашей аудиторий, что неизменно повысит узнаваемость и принесет дополнительные продажи. Построим стратегию, которая позволит: повысить рост лояльности текущих и потенциальных клиентов сформирование имиджа бренда проинформировать аудиторию провести анализ обратной связи от пользователей отработать негативные отзывы и собрать положительные Для продвижения бренда используем такие площадки, как ВКонтакте, Facebook, Instagram, Одноклассники, LinkedIn, Twitter, YouTube и др. Секрет нашего успеха в индивидуальном подходе</span>
+            <a href="#" class="smm-button-link">Автоматизировать процессы</a>
+        </div>
+        <img class="smm-info-1-img" src="images/crm-info-1-img.jpg">
+    </div>
+</div>
+</section>
+<section class="smm-info-2">
+<div class="container container-smm-info-1 container-smm-info-2">
+    <div class="wrapper-smm-info-1">
+    <img class="smm-info-2-img" src="images/crm-info-2-img.jpg">
+        <div class="wrapper-smm-info-1-text wrapper-smm-info-2-text">
+            <h4 class="title smm-info-1-title">Акцент на сделке</h4>
+            <span class="smm-info-1-text">Social Media Marketing - это комплекс инструментов, позволяющий найти и привлечь внимание целевой аудитории. Основная цель SMM повысить лояльность бренда перед вашей аудиторий, что неизменно повысит узнаваемость и принесет дополнительные продажи. Построим стратегию, которая позволит: повысить рост лояльности текущих и потенциальных клиентов сформирование имиджа бренда проинформировать аудиторию провести анализ обратной связи от пользователей отработать негативные отзывы и собрать положительные Для продвижения бренда используем такие площадки, как ВКонтакте, Facebook, Instagram, Одноклассники, LinkedIn, Twitter, YouTube и др. Секрет нашего успеха в индивидуальном подходе</span>
+            <a href="#" class="smm-button-link">Автоматизировать процессы</a>
+        </div>  
+    </div>
+</div>
+</section>
+<section class="smm-info-1">
+<div class="container container-smm-info-1">
+    <h4 class="title smm-info-1-title">Широкие возможности интеграций</h4>
+    <div class="wrapper-smm-info-1">
+        <div class="wrapper-smm-info-1-text">
+            <span class="smm-info-1-text">Social Media Marketing - это комплекс инструментов, позволяющий найти и привлечь внимание целевой аудитории. Основная цель SMM повысить лояльность бренда перед вашей аудиторий, что неизменно повысит узнаваемость и принесет дополнительные продажи. Построим стратегию, которая позволит: повысить рост лояльности текущих и потенциальных клиентов сформирование имиджа бренда проинформировать аудиторию провести анализ обратной связи от пользователей отработать негативные отзывы и собрать положительные Для продвижения бренда используем такие площадки, как ВКонтакте, Facebook, Instagram, Одноклассники, LinkedIn, Twitter, YouTube и др. Секрет нашего успеха в индивидуальном подходе</span>
+            <a href="#" class="btn-more-service-seo smm-button-link" >Оценить вохможность</a>
+        </div>
+        <img class="smm-info-1-img" src="images/crm-info-3-img.jpg">
+    </div>
+</div>
+</section>
+<section class="smm-info-2">
+<div class="container container-smm-info-1 container-smm-info-2">
+    <div class="wrapper-smm-info-1">
+    <img class="smm-info-4-img" src="images/crm-info-4-img.jpg">
+        <div class="wrapper-smm-info-1-text wrapper-smm-info-2-text">
+            <h4 class="title smm-info-1-title">Ваша атмосфера и телефония в одной связке</h4>
+            <span class="smm-info-1-text">Social Media Marketing - это комплекс инструментов, позволяющий найти и привлечь внимание целевой аудитории. Основная цель SMM повысить лояльность бренда перед вашей аудиторий, что неизменно повысит узнаваемость и принесет дополнительные продажи. Построим стратегию, которая позволит: повысить рост лояльности текущих и потенциальных клиентов сформирование имиджа бренда проинформировать аудиторию провести анализ обратной связи от пользователей отработать негативные отзывы и собрать положительные Для продвижения бренда используем такие площадки, как ВКонтакте, Facebook, Instagram, Одноклассники, LinkedIn, Twitter, YouTube и др. Секрет нашего успеха в индивидуальном подходе</span>
+            <a href="#" class="smm-button-link">Протестировать бесплатно</a>
+        </div>  
+    </div>
+</div>
+</section>
+<section class="smm-info-5">
+<div class="container container-smm-info-1">
+    <div class="wrapper-smm-info-1">
+        <div class="wrapper-smm-info-1-text wrapper-smm-info-5-text">
+            <h4 class="title smm-info-1-title">Аналитика продаж</h4>
+            <ul class="smm-info-5-text-list" >
+                <li class="smm-info-1-text smm-info-5-text-item">качественная подборка семантического ядра и грамматиные настройки. которые позволят даже молодым сайтам конкурировать с более изместными представителями тое же ниши;</li>
+                <li class="smm-info-1-text smm-info-5-text-item">прирост целевых посвящений из поиска "Яндека", которые частично конверируются в продажи;</li>
+                <li class="smm-info-1-text smm-info-5-text-item">создание индивидуальной рекламной стратегии. учитывающей особенности онкретного проекта;</li>
+                <li class="smm-info-1-text smm-info-5-text-item">рацональное использование бюджета;</li>
+            </ul>
+            <a href="#" class="smm-button-link" >Аналитика продаж</a>
+        </div>
+        <img class="smm-info-5-img" src="images/crm-info-5-img.jpg">
+    </div>
+</div>
+</section>
+
+<script src="./js/jquery-3.4.1.min.js"></script>
+<script src="./js/jquery.paroller.min.js"></script>
+<script defer type="text/javascript" src="js/slick.min.js"></script>
+<script defer src="./main.js"></script>     
+</body>
+</html>
