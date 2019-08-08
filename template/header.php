5a62f6c6131e3fd02eb890853bf434290c6f47ad
Alex <work.alex966@gmail.com>
2019-08-08 20:31:11 +0300

    новая менюха

--- template/header.php
+++ template/header.php
@@ -29,14 +29,14 @@
             <svg xmlns="http://www.w3.org/2000/svg" width="28" height="19"><path fill="#fff" paint-order="stroke fill markers" fill-rule="evenodd" d="M1 1h26v3H1V1zm0 7h26v3H1V8zm0 7h26v3H1v-3z"></path></svg>
         </button>
         <a href="index.php" class="header-mobile-logo">
-            <img src="./images/logo-small.png" class="logo__img header-mobile-logo__img" alt="Increase - веб-агенство">
+            <img src="./images/logo-small.png" class=" header-mobile-logo__img" alt="Increase - веб-агенство">
         </a>
     </section>
     <section class="mobile-menu ">
         <div class="mobile-menu-header">
             <button type="button" class="close-menu-btn">
                 <span class="sr-only">Закрыть меню</span>
-                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="19" fill="rgb(31,45,85)" viewBox="0 0 47.971 47.971"><path d="M28.228 23.986L47.092 5.122a2.998 2.998 0 0 0 0-4.242 2.998 2.998 0 0 0-4.242 0L23.986 19.744 5.121.88a2.998 2.998 0 0 0-4.242 0 2.998 2.998 0 0 0 0 4.242l18.865 18.864L.879 42.85a2.998 2.998 0 1 0 4.242 4.241l18.865-18.864L42.85 47.091c.586.586 1.354.879 2.121.879s1.535-.293 2.121-.879a2.998 2.998 0 0 0 0-4.242L28.228 23.986z"></path></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="19" fill="#fff" viewBox="0 0 47.971 47.971"><path d="M28.228 23.986L47.092 5.122a2.998 2.998 0 0 0 0-4.242 2.998 2.998 0 0 0-4.242 0L23.986 19.744 5.121.88a2.998 2.998 0 0 0-4.242 0 2.998 2.998 0 0 0 0 4.242l18.865 18.864L.879 42.85a2.998 2.998 0 1 0 4.242 4.241l18.865-18.864L42.85 47.091c.586.586 1.354.879 2.121.879s1.535-.293 2.121-.879a2.998 2.998 0 0 0 0-4.242L28.228 23.986z"></path></svg>
             </button>
         </div>
         <div class="mobile-menu-content">
