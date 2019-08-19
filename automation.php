8be65e3ed191d227de9d1f3ace5c41aab21ec0a8
Alex <work.alex966@gmail.com>
2019-08-19 17:02:47 +0300

    страница автоматизация

---
+++ automation.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="./style.css">
+    <link rel="stylesheet" type="text/css" href="css/slick.css"/>
+    <title>Automation</title>
+</head>
+<body>
+<header class="header-land-services">
+    <?php include('template/header.php') ?>
+<img src="./images/free-audit-blot.png" class="paroller header__paroller-1" data-paroller-factor="0.2" aria-hidden="true" alt="" style="transform: translate(0px, 21px); transition: transform 0.1s ease 0s; will-change: transform;">
+</header>
+<section class="automation-about">
+    <img class="automation-about-fon-2" src="images/automation-about-fon-2.png">
+    <div class="container container-automation-about">
+    <h4 class="section-title automation-about-title">Автоматизация</h4>
+        <div class="small-info-service-about small-info-service-automation-about">
+            <img class="automation-about-title-img" src="images/automatic-about-img.png">
+            <div class="sml-info-service-we-top sml-info-service-automation-we-top">amoCRM – полный комплекс инструментов,
+направленных на раскрытие потенциала вашего
+отдела продаж и повышение его эффективности</div>
+            <button class="btn-more-service-automation cost">Подробнее</button>
+        </div>
+    </div>
+</section>
+<section class="automation-info">
+    <div class="container container-automation-info">
+        <img class="automation-info-img" src="images/automatic-info-img-1.png">
+        <div class="wrapper-automation-info">
+            <img class="automation-info-title-img" src="images/automatic-info-img-title-1.png">
+            <span class="section-text automation-info-text">Система сквозной бизнес-аналитики Roistat собирает данные из CRM системы, рекламных площадок и вашего сайта. За секунды строит отчеты по 30+ бизнес-показателям.</span>
+            <span class="section-text automation-info-title-list">Позволит: </span>
+            <ul class="automation-info-list">
+                <li class="automation-info-item section-text">Сократить рекламный бюджет</li>
+                <li class="automation-info-item">Найти и отключить рекламные каналы</li>
+                <li class="automation-info-item">Увидеть все точки касания клиента с продуктом</li>
+                <li class="automation-info-item">Использовать инструменты для роста продаж</li>
+            </ul>
+            <button class="cost">Заказать</button>
+        </div>
+    </div>
+</section>
+<section class="automation-info">
+    <div class="container container-automation-info">
+        <div class="wrapper-automation-info">
+            <img class="automation-info-title-img-2" src="images/automatic-info-img-title-2.png">
+            <span class="section-text automation-info-text one-c-text">Social Media Marketing - это комплекс инструментов, позволяющий найти и привлечь внимание целевой аудитории. Основная цель SMM повысить лояльность бренда перед вашей аудиторий, что неизменно повысит узнаваемость и принесет дополнительные продажи. Построим стратегию, которая позволит: повысить рост лояльности текущих и потенциальных клиентов сформирование</span>
+            <button class="cost">Заказать</button>
+        </div>
+        <img class="automation-info-img" src="images/automatic-info-img-2.png">
+    </div>
+</section>
+<section class="automation-info">
+    <div class="container container-automation-info">
+        <img class="automation-info-title-img" src="images/automatic-info-img-3.png">
+        <div class="wrapper-automation-info">
+            <h4 class="section-title automation-info-title">Ваша атмосфера и телефония в одной связке</h4>
+            <span class="section-text automation-info-text voip-text">Roistat <br> Создаем, ведем и поддерживаем присутствие вашего бренда в социальных сетях</span>
+            <button class="cost">Заказать</button>
+        </div>
+    </div>
+</section>
+<section class="price">
+    <?php include('template/brif.php') ?>
+    
+        <img src="./images/free-audit-blot.png" class="paroller price__paroller-1" data-paroller-factor="0.2" aria-hidden="true" alt="" style="transform: translate(0px, 21px); transition: transform 0.1s ease 0s; will-change: transform;">
+        <img src="./images/price-blot-1.png" class="paroller price__paroller-2" data-paroller-factor="0.2" aria-hidden="true" alt="" style="transform: translate(0px, 21px); transition: transform 0.1s ease 0s; will-change: transform;">
+        <img src="./images/free-audit-blot-3.png" class="paroller price__paroller-3" data-paroller-factor="0.2" aria-hidden="true" alt="" style="transform: translate(0px, 21px); transition: transform 0.1s ease 0s; will-change: transform;">
+    </section>
+    <footer class="footer">
+        <?php include('template/footer.php') ?>
+    </footer>
+<script src="./js/jquery-3.4.1.min.js"></script>
+<script src="./js/jquery.paroller.min.js"></script>
+<script defer type="text/javascript" src="js/slick.min.js"></script>
+<script defer src="./main.js"></script>     
+</body>
+</html>
